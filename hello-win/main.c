0d14397964c86cef4771ffc04854712410fdf612
chenzhengyong <chenzhengyong@uniontech.com>
2026-01-29 16:56:18 +0800

    修改 hello-win 示例，窗口居中显示

--- hello-win/main.c
+++ hello-win/main.c
@@ -1,31 +1,71 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    switch(msg) {
-        case WM_CLOSE: PostQuitMessage(0); break;
-        default: return DefWindowProc(hwnd, msg, wParam, lParam);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg) {
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        FillRect(hdc, &ps.rcPaint,
+                 (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nCmdShow) {
+                   LPSTR lpCmdLine, int nCmdShow)
+{
     WNDCLASS wc = {0};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInstance;
     wc.lpszClassName = "MyWindowClass";
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindow("MyWindowClass", "Hello Wine", WS_OVERLAPPEDWINDOW,
-                             CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
-                             NULL, NULL, hInstance, NULL);
+    const int clientWidth  = 400;
+    const int clientHeight = 300;
+    const DWORD style = WS_OVERLAPPEDWINDOW;
+
+    RECT rc = {0, 0, clientWidth, clientHeight};
+    AdjustWindowRect(&rc, style, FALSE);
+
+    int winWidth  = rc.right - rc.left;
+    int winHeight = rc.bottom - rc.top;
+
+    int screenWidth  = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    int x = (screenWidth  - winWidth)  / 2;
+    int y = (screenHeight - winHeight) / 2;
+
+    HWND hwnd = CreateWindow(
+        "MyWindowClass",
+        "Hello Wine",
+        style,
+        x, y,
+        winWidth, winHeight,
+        NULL, NULL, hInstance, NULL
+    );
 
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     MSG msg;
-    while(GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
     return 0;
 }
+
